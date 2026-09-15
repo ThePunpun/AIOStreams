0fcd5821b9c623626714b618001f155b73ecfc00
ThePunpun <kevinedward985@gmail.com>
2026-09-15 10:48:52 -0400

    feat: add optional yearless movie search fallback (#1305)

--- packages/core/src/config/index.ts
+++ packages/core/src/config/index.ts
@@ -49,6 +49,7 @@ const builtinsSchemaWithTitleLimits = {
     titleLimits: titleLimitsField,
     queryConcurrency: builtinsSchema.scrape.queryConcurrency,
     latinQueriesOnly: builtinsSchema.scrape.latinQueriesOnly,
+    yearlessMovieFallback: builtinsSchema.scrape.yearlessMovieFallback,
     dateBased: builtinsSchema.scrape.dateBased,
     absoluteSearch: builtinsSchema.scrape.absoluteSearch,
   },
